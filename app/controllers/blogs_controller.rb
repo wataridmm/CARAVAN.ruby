61b0cb7eae0b05bc4ab866796256b44e5400be8f
wataridmm <r.watari.watari@gmail.com>
2021-09-09 08:33:40 +0000

    add

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,16 +1,27 @@
 class BlogsController < ApplicationController
-  def index
-  end
-
   def show
+  end
+  
+  def index
+    @blogs = Blog.all
+    
   end
 
   def new
+    @blog = Blog.new
   end
-  
+
   def create
-    
+    blog = Blog.new(blog_params)
+    blog.save
+    redirect_to blogs_path
+  end
 
   def edit
   end
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :category, :body)
+  end
+
 end
